092495ba5ee2e4870835da54a9a91ffc9055e346
Diego Iastrubni <diegoiast@gmail.com>
2018-12-20 12:57:59 +0200

    Initial Commit

---
+++ test1-ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  test1-ios
+//
+//  Created by diego on 20/12/2018.
+//  Copyright © 2018 Diego. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
